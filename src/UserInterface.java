08defa3ebc3c0a27447b1a34b4af5b3c73586bd3
theosmuller <theosmuller@gmail.com>
2022-05-03 01:02:56 -0300

    adding midi saving option

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,7 +1,11 @@
+import org.jfugue.midi.MidiFileManager;
+import org.jfugue.pattern.Pattern;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
 
 public class UserInterface implements IUserInterface {
 
@@ -10,6 +14,7 @@ public class UserInterface implements IUserInterface {
     private final JButton helpButtonWidget = new JButton("Help");
     private final JButton playButtonWidget = new JButton("Play");
     private final JButton stopButtonWidget = new JButton("Stop");
+    private final JButton saveButtonWidget = new JButton("Save");
     private final JTextArea userInputWidget = new JTextArea();
 
     public void start(){
@@ -24,39 +29,36 @@ public class UserInterface implements IUserInterface {
         helpButtonWidget.setBounds(10, 220, 130, 30);
         playButtonWidget.setBounds(150, 220, 130, 30);
         stopButtonWidget.setBounds(150, 220, 130, 30);
+        saveButtonWidget.setBounds(10, 220, 130, 30);
         mainWindow.add(helpButtonWidget);
         mainWindow.add(playButtonWidget);
         mainWindow.add(stopButtonWidget);
+        mainWindow.add(saveButtonWidget);
         setUpButtonListeners();
     }
 
     private void setUpButtonListeners(){
-        ActionListener helpButtonListener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "help");
-            }
+        ActionListener helpButtonListener = e -> JOptionPane.showMessageDialog(null, "help");
+        ActionListener playButtonListener = e -> {
+            playButtonWidget.setVisible(false);
+            stopButtonWidget.setVisible(true);
+            helpButtonWidget.setVisible(false);
+            saveButtonWidget.setVisible(true);
+            startPlay();
         };
-        ActionListener playButtonListener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                playButtonWidget.setVisible(false);
-                stopButtonWidget.setVisible(true);
-                startPlay();
-            }
+        ActionListener stopButtonListener = e -> {
+            stopButtonWidget.setVisible(false);
+            playButtonWidget.setVisible(true);
+            helpButtonWidget.setVisible(false);
+            saveButtonWidget.setVisible(true);
+            programState.pause();
         };
-        ActionListener stopButtonListener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                stopButtonWidget.setVisible(false);
-                playButtonWidget.setVisible(true);
-                programState.pause();
-            }
-        };
+        ActionListener saveButtonListener = e -> onSaveButtonPressed();
 
         helpButtonWidget.addActionListener(helpButtonListener);
         playButtonWidget.addActionListener(playButtonListener);
         stopButtonWidget.addActionListener(stopButtonListener);
+        saveButtonWidget.addActionListener((saveButtonListener));
     }
 
     private void startPlay(){
@@ -74,6 +76,8 @@ public class UserInterface implements IUserInterface {
     public void playEnded(){
         this.stopButtonWidget.setVisible(false);
         this.playButtonWidget.setVisible(true);
+        helpButtonWidget.setVisible(true);
+        saveButtonWidget.setVisible(false);
     }
     private void onPauseButtonClicked() {
         if (programState.getPaused()) {
@@ -81,6 +85,17 @@ public class UserInterface implements IUserInterface {
         }
         else{
             programState.unpause();
+        }
+    }
+
+    private void onSaveButtonPressed() {
+        // Our patterns…
+        try {
+            File filePath = new File("./MIDI/track.midi");
+            MidiFileManager.savePatternToMidi(new Pattern(programState.getMusicText()), filePath);
+            JOptionPane.showMessageDialog(null, "Saved song to MIDI");
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
